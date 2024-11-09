e0e5c3e4bcee79eef545816cb7b2286537508983
mredwan <mredwan.kemal@gmail.com>
2024-11-09 10:50:07 +0300

    finished upading the new ui styles

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ let myInput = document.getElementById("my-input");
 
 
 addButton.addEventListener("click",function(e){
-    let newElement=document.createElement('li');
+    let newElement=document.createElement('div');
     newElement.classList.add("task-item");
-    newElement.innerText = myInput.value;
     todoList.appendChild(newElement);
+    let newList = document.createElement("li");
+    newList.classList.add("item");
+    newList.innerText = myInput.value;
+    newElement.appendChild(newList);
+    let trashCan = document.createElement("button");
+    trashCan.classList.add("trash-can");
+    trashCan.innerHTML ='<i class="fa fa-trash-can fa-lg"></i>';
+    newElement.appendChild(trashCan);
+    let checkBox= document.createElement("button");
+    checkBox.classList.add("check-box");
+    checkBox.innerHTML='<i class="fa fa-check-square fa-lg"></i>';
+    newElement.appendChild(checkBox);
     myInput.value="";
 
-    newElement.addEventListener("click",function(e){
-       e.target.classList.add("fall");
-       e.target.addEventListener("transitionend", function(){
-        e.target.classList.add("remove");
-       });
-    })
 
     e.preventDefault();
 });
+
